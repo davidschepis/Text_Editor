669f9e781781874c03e9e9295e4ddb13cbf3df06
David Schepis <davidschepis@gmail.com>
2022-02-11 13:00:30 -0700

    fixed bugs, app now fully functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,35 +4,37 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        //console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      //console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the database');
+  //console.log('Post to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.add({ todo: content });
+  const store = tx.objectStore ('jate');
+  const request = store.put({content: content});
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  //console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET all from the database');
+  //console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  if (result.length === 0) {
+    return null;
+  }
+  return result[result.length-1].content;
 };
 
 initdb();
